2f3863129f5622e3eefdc85e51ed8810c089d2b3
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2021-02-05 21:59:38 -0500

    configuring file structure and intall stylecompnents

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from "styled-components";
+import { theme } from './themes/theme.js'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider theme={theme} >
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>
+  ,
   document.getElementById('root')
 );
 
