18abf1c67686f363215b9d9641ba36ed5335bdd1
kirbycreese <70101203+kirbycreese@users.noreply.github.com>
2021-02-05 19:29:14 -0500

    cleaning up boiler plate and creating components folder with Header file

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Button from 'react-bootstrap/Button';
+
+export class Header extends Component {
+    render() {
+        return (
+            <div>
+                <Button variant="primary">Primary</Button>{' '}
+            </div>
+        )
+    }
+}
+
+export default Header
